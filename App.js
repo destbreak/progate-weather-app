f9f4bc5fa6644470e342f9323f810445e48ad677
Reynaldi Hadiansyah <48123410+destbreak@users.noreply.github.com>
2024-06-24 00:19:45 +0700

    Create search function and setup environment variables

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import React from "react";
+import axios from "axios";
 import { StyleSheet, View } from "react-native";
 import WeatherSearch from "./src/components/weatherSearch";
 import WeatherInfo from "./src/components/weatherInfo";
+import { API_KEY } from "@env";
 
 export default function App() {
+  const searchWeather = (location) => {
+    const url = "https://api.openweathermap.org/data/2.5/weather";
+
+    axios
+      .get(`${url}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <WeatherSearch />
+      <WeatherSearch searchWeather={searchWeather} />
       <WeatherInfo />
     </View>
   );
